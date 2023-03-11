206aef5cc949b91b475f68b5bd0b1256fd99bb4d
Juan Salazar <ferthelet@gmail.com>
2023-03-10 19:29:48 -0600

    adds text and id to newHeader, id not working

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 // takes main out
 const main = document.getElementsByTagName("main")[0];
 main.remove();
+// new header
+const newHeader = document.createElement("h1");
+// id of 'victory'
+const id = document.createElement("id");
+id.textContent = "victory";
+// champion text 
+newHeader.textContent = "Juan Salazar is the champion";
+document.body.append(newHeader);
+newHeader.append(id);
