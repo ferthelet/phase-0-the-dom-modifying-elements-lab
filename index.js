2d0453d5797e60a94d3fce7ff3aa255945a5f30b
Juan Salazar <ferthelet@gmail.com>
2023-03-10 19:39:04 -0600

    adds id with setAttribute, nstill not working

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Write your code here!
+
 // takes main out
 const main = document.getElementsByTagName("main")[0];
 main.remove();
+
 // new header
 const newHeader = document.createElement("h1");
-// id of 'victory'
-const id = document.createElement("id");
-id.textContent = "victory";
+
 // champion text 
 newHeader.textContent = "Juan Salazar is the champion";
 document.body.append(newHeader);
-newHeader.append(id);
+newHeader.setAttribute("id", "victory")
